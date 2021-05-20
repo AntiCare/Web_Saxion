bf74eca31042a5cec959d995f7cce3ca8043f865
Mykhailo Galenda <m.galenda@fundaments.nl>
2021-05-21 00:39:17 +0200

    change JSON db to sqlite db

--- vue-app3/server.js
+++ vue-app3/server.js
@@ -1,16 +1,16 @@
-var fs = require('fs');
-var path = require('path');
 var express = require('express');
 var bodyParser = require('body-parser');
 var app = express();
-
-var PRODUCTS_FILE = path.join(__dirname, 'src/assets/js/components/product-data.json');
+var data = require('./db/db')
 
 app.set('port', (process.env.PORT || 3000));
 
 app.use('/', express.static(__dirname));
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({extended: true}));
+
+
+
 
 // Additional middleware which will set headers that we need on each request.
 app.use(function(req, res, next) {
@@ -25,120 +25,33 @@ app.use(function(req, res, next) {
     next();
 });
 
-app.get('/api/products', function(req, res) {
-    fs.readFile(PRODUCTS_FILE, function(err, data) {
-        if (err) {
-            console.error(err);
-            process.exit(1);
-        }
-        res.json(JSON.parse(data));
-    });
+app.get('/api/products', function(req, res, next) {
+    data.returnAllProducts(res,next);
 });
 
 
-app.get('/api/product/:id', function(req, res) {
-
-    fs.readFile(PRODUCTS_FILE, function(err, data) {
-        if (err) {
-            console.error(err);
-            process.exit(1);
-        }
-
-        var json = JSON.parse(data);
-
-        for(var i = 0; i <= json.length; i++)
-        {
-            if(json[i]['id'] == req.params.id)
-            {
-                res.json(json[i]);
-                break;
-            }
-        }
-    });
+app.get('/api/product/:id', function(req, res, next) {
+    data.returnProduct(res, next,req.params.id);
 });
 
-app.post('/api/product/create', function(req, res) {
-
-    fs.readFile(PRODUCTS_FILE, function(err, data) {
-        if (err) {
-            console.error(err);
-            process.exit(1);
-        }
-        var products = JSON.parse(data);
-
-        var newProduct = {
-            id: Date.now(),
-            name: req.body.name,
-            price: req.body.price,
-        };
-        products.push(newProduct);
-        fs.writeFile(PRODUCTS_FILE, JSON.stringify(products, null, 4), function(err) {
-            if (err) {
-                console.error(err);
-                process.exit(1);
-            }
-            res.json(products);
-        });
-    });
+app.post('/api/product/create', function(req, res, next) {
+    var newProduct = {
+        name: req.body.name,
+        price: req.body.price,
+    };
+    data.createProduct(res, next, newProduct)
 });
 
-app.patch('/api/product/edit/:id', function(req, res) {
-    fs.readFile(PRODUCTS_FILE, function(err, data) {
-        if (err) {
-            console.error(err);
-            process.exit(1);
-        }
-        var products = JSON.parse(data);
-
-        for(var i = 0; i <= products.length; i++)
-        {
-            if(products[i]['id'] == req.params.id)
-            {
-                var product = products[i];
-                product.name = req.body.name;
-                product.price = req.body.price;
-
-                products.splice(i, 1);
-                products.push(product);
-
-                fs.writeFile(PRODUCTS_FILE, JSON.stringify(products, null, 4), function(err) {
-                    if (err) {
-                        console.error(err);
-                        process.exit(1);
-                    }
-                    res.json(products);
-                });
-                break;
-            }
-        }
-    });
+app.patch('/api/product/edit/:id', function(req, res, next) {
+    var updatedProduct = {
+        name: req.body.name,
+        price: req.body.price,
+    };
+    data.updateProduct(res, next, req.params.id, updatedProduct)
 });
 
-app.delete('/api/product/delete/:id', function(req, res) {
-    fs.readFile(PRODUCTS_FILE, function(err, data) {
-        if (err) {
-            console.error(err);
-            process.exit(1);
-        }
-        var products = JSON.parse(data);
-
-        for(var i = 0; i <= products.length; i++)
-        {
-            if(products[i]['id'] == req.params.id)
-            {
-                products.splice(i, 1);
-
-                fs.writeFile(PRODUCTS_FILE, JSON.stringify(products, null, 4), function(err) {
-                    if (err) {
-                        console.error(err);
-                        process.exit(1);
-                    }
-                    res.json(products);
-                });
-                break;
-            }
-        }
-    });
+app.delete('/api/product/delete/:id', function(req, res, next) {
+    data.deleteProduct(res, next, req.params.id);
 });
 
 
