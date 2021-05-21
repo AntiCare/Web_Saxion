e2dea57d246cec12297c5128afc21cf476be0eb3
k <contact@mywebartist.eu>
2021-05-21 18:36:04 +0200

    update

--- vue-keith/quiz2/src/App.vue
+++ vue-keith/quiz2/src/App.vue
@@ -1,15 +1,21 @@
 <template>
   <div id="app">
-    <Header />
+    
+    <Header :numCorrect="numCorrect" :numTotal="numTotal" />
+    
 
     <b-container class="bv-example-row">
       <b-row>
         <b-col sm="6" offset="3">
           <QuestionBox
-          v-if="questions.length"
-           :currentQuestion="questions[index]"
-          :nextQuestion="nextQ"
-           />
+             v-if="questions.length"
+            :currentQuestion="questions[index]"
+            :nextQuestion="nextQ"
+            :increment="increment"
+            :start="start"
+            :correctAnswersCount="numCorrect"
+            :index="index"
+          />
         </b-col>
       </b-row>
     </b-container>
@@ -30,26 +36,46 @@ export default {
     return {
       questions: [],
       index: 0,
+      numCorrect: 0,
+      numTotal: 1,
     };
   },
   methods: {
     nextQ: function() {
       this.index++;
+      this.numTotal++;
       // console.log('next q')
+    },
+    increment(isCorrect) {
+      if (isCorrect) {
+        this.numCorrect++;
+      }
+      // this.numTotal++;
+    },
+    start() {
+          this.resetStats();
+      fetch("https://opentdb.com/api.php?amount=10&category=18&type=multiple", {
+        method: "get",
+      })
+        .then((response) => {
+          // console.log(response.json())
+          return response.json();
+        })
+        .then((jsonData) => {
+          this.questions = jsonData.results;
+      
+        });
+    },
+    resetStats: function() {
+      this.questions = [];
+      this.index = 0;
+      this.numCorrect = 0;
+      this.numTotal = 1;
     },
   },
 
   mounted: function() {
-    fetch("https://opentdb.com/api.php?amount=10&category=18&type=multiple", {
-      method: "get",
-    })
-      .then((response) => {
-        // console.log(response.json())
-        return response.json();
-      })
-      .then((jsonData) => {
-        this.questions = jsonData.results;
-      });
+    this.start();
   },
 };
 </script>
--- vue-keith/quiz2/src/components/Header.vue
+++ vue-keith/quiz2/src/components/Header.vue
@@ -1,8 +1,27 @@
 <template>
-  <div>
+  <!-- <div>
     <b-nav tabs>
-      <b-nav-item disabled>Fancy Quiz App</b-nav-item>
-      <b-nav-item disabled>Counter: 4/10</b-nav-item>
+      <b-nav-item  href="/">Fancy Quiz App</b-nav-item>
+      <b-nav-item disabled>Score of {{ numCorrect }} from {{ numTotal }} questions</b-nav-item>
+    </b-nav>
+  </div> -->
+
+  <div>
+    <b-navbar toggleable="sm" type="dark" variant="dark">
+      <b-navbar-brand href="/" class="mx-auto">
+        Fancy Quiz App
+      </b-navbar-brand>
+    </b-navbar>
+    <b-nav tabs class="mx-auto">
+      <b-nav-item disabled
+        >Score of {{ numCorrect }} from {{ numTotal }} questions</b-nav-item
+      >
     </b-nav>
   </div>
 </template>
+
+<script>
+export default {
+  props: ["numCorrect", "numTotal"],
+};
+</script>
