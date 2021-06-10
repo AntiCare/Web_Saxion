d39d79bcf7e7b6f66e329c2d74c2def854cdf7a3
yang Cheng <474340@student.saxion.nl>
2021-06-10 19:22:55 +0200

    -add exam-score into SQLite, use api get data.

--- vue-app3-back/routes/index.js
+++ vue-app3-back/routes/index.js
@@ -27,6 +27,18 @@ let abb = (async () => {
 //     "insert into EXAM_SCHEDULE (id, exam_name, exam_time) values (5, 'dev tools 5', '1623266926');\n"
 // )
 
+//     await db.exec("CREATE TABLE IF NOT EXISTS exam_score (\n" +
+//     "\tid INT,\n" +
+//     "\texam_name VARCHAR(50),\n" +
+//     "\texam_result INT\n" +
+//     ");\n" +
+//     "insert into EXAM_SCORE (id, exam_name, exam_result) values (1, 'dev tools', 5);\n" +
+//     "insert into EXAM_SCORE (id, exam_name, exam_result) values (2, 'dev tools 2', 6);\n" +
+//     "insert into EXAM_SCORE (id, exam_name, exam_result) values (3, 'dev tools 3', 7);\n" +
+//     "insert into EXAM_SCORE (id, exam_name, exam_result) values (4, 'dev tools44', 8);\n" +
+//     "insert into EXAM_SCORE (id, exam_name, exam_result) values (5, 'dev tools 5', 9);\n"
+// )
+
 // await db.exec('INSERT INTO tbl VALUES ("test")');
 // let result =  db.get("SELECT col FROM tbl WHERE col = ?", "test");
 // console.log(result)
@@ -94,6 +106,16 @@ router.get("/api/showdummy", async function (req, res, next) {
 router.get("/api/exam-schedule", async function (req, res, next) {
     try {
         let result = await db.all("SELECT * FROM exam_schedule");
+        res.json(result);
+    } catch (e) {
+        res.json(e);
+    }
+});
+
+// Show exam score data
+router.get("/api/exam-score", async function (req, res, next) {
+    try {
+        let result = await db.all("SELECT * FROM exam_score");
         res.json(result);
     } catch (e) {
         res.json(e);
