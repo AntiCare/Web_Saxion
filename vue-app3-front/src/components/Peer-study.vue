5a6bdbd50e6847ff516697325956145a1579e975
Mykhailo Galenda <m.galenda@fundaments.nl>
2021-06-22 14:46:04 +0200

    divide component into smaller pieces

--- vue-app3-front/src/components/Peer-study.vue
+++ vue-app3-front/src/components/Peer-study.vue
@@ -1,39 +1,36 @@
 <template>
   <div>
-    <v-card
-      class="pa-2 mb-4"
-      outlined
-      elevation="0"
-      shaped
-      v-for="(assignment, idx) in assignments"
-    >
-      <v-card-title>{{ assignment.title }}</v-card-title>
-      <v-card-subtitle>{{ assignment.subtitle }}</v-card-subtitle>
-      <v-btn
-        color="primary"
-        text
-        @click="assignment.viewUploadPoint = true"
-      >
-        VIEW
-      </v-btn>
-      <div v-if="assignment.viewUploadPoint">
-        <Peer-study-assignment-view></Peer-study-assignment-view>
-      </div>
-    </v-card>
+    <div v-for="( assignment, idx ) in assignments">
+      <PeerStudyAssignment v-bind:assignment="assignment" v-bind:index="idx"></PeerStudyAssignment>
+    </div>
   </div>
 </template>
 
 <script>
 import PeerStudyAssignmentView from "@/components/peer-study/Peer-study-assignment-view";
+import PeerStudyAssignment from "@/components/peer-study/Peer-study-assignment-preview";
+
 export default {
   name: "Peer-study",
-  components: {PeerStudyAssignmentView},
+  components: {PeerStudyAssignment, PeerStudyAssignmentView},
   data() {
     return {
       files: [],
       loading: false,
       icon: 'mdi-forum',
       assignments: [
+        {
+          title: 'Research update',
+          subtitle: 'some subtitle',
+          viewUploadPoint: false
+
+        },
+        {
+          title: 'Research update',
+          subtitle: 'some subtitle',
+          viewUploadPoint: false
+
+        },
         {
           title: 'Research update',
           subtitle: 'some subtitle',
