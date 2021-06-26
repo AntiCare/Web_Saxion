405a2861aa471a7daf1b3dc4cf0c95ae7fee2f5e
yang Cheng <474340@student.saxion.nl>
2021-06-26 13:16:43 +0200

    - delete unused api.

--- vue-app3-back/routes/index.js
+++ vue-app3-back/routes/index.js
@@ -256,18 +256,6 @@ let abb = (async () => {
 
 })()
 
-
-/* GET home page. */
-router.get('/', function (req, res, next) {
-    res.send({message: 'Student portal backend server'});
-});
-
-router.get('/test', function (req, res, next) {
-    res.send({message: 'Student portal backend server'});
-});
-
-
-
 //get user login data.
 router.post('/api/login', async function (req, res) {
     //get email & password
@@ -301,26 +289,6 @@ router.post('/api/register', async function (req, res) {
     console.log(password.toString());
     //return rep to frontend.
     res.send("200");
-});
-
-
-// dummy test for sql lite
-router.get("/api/enterdummy", async function (req, res, next) {
-
-    let result = await db.exec('INSERT INTO tbl VALUES ("sssssx")');
-    // console.log(result);
-    // return result
-
-    res.json({"status": "entered data in tbl"});
-});
-
-// dummy test for sql lite
-router.get("/api/showdummy", async function (req, res, next) {
-    let result = await db.all("SELECT * FROM tbl");
-    // console.log(result);
-    // return result
-
-    res.json(result);
 });
 
 // Show exam schedule data
@@ -559,38 +527,4 @@ router.get("/api/lectures-archive", async function (req, res, next) {
         res.json(e);
     }
 });
-
-
-
-
-router.get('/api/products', function (req, res, next) {
-    data.returnAllProducts(res, next);
-});
-
-
-router.get('/api/product/:id', function (req, res, next) {
-    data.returnProduct(res, next, req.params.id);
-});
-
-router.post('/api/product/create', function (req, res, next) {
-    var newProduct = {
-        name: req.body.name,
-        price: req.body.price,
-    };
-    data.createProduct(res, next, newProduct)
-});
-
-router.patch('/api/product/edit/:id', function (req, res, next) {
-    var updatedProduct = {
-        name: req.body.name,
-        price: req.body.price,
-    };
-    data.updateProduct(res, next, req.params.id, updatedProduct)
-});
-
-router.delete('/api/product/delete/:id', function (req, res, next) {
-    data.deleteProduct(res, next, req.params.id);
-});
-
-
 module.exports = router;
